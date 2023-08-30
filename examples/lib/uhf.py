58bf74d49cbfca7dc1d9b05b31a933301e000a6b
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-08-30 12:28:53 +0530

    Update uhf.py
    Added more commands, setting region and read transmit power

--- examples/lib/uhf.py
+++ examples/lib/uhf.py
@@ -9,12 +9,27 @@ standard commands for UHF operations, refer command manual for more details
 https://github.com/sbcshop/UHF_Reader_Pico_W_Software/blob/main/documents/UHF%20Commands%20Manual.pdf
 So, you can add more commands for other operation
 '''
+
 STARTBYTE     ='BB00'	# combine Header + Type
 ENDBYTE       ='7E'
+
+'''2.1 Get the reader module information'''
 HARD_VERSION  ='0300010004'
-MULTIPLE_READ ='27000322271083'
+
+''' 2.2 Single polling command '''
 SINGLE_READ   ='22000022'
+
+''' 2.3 Several times polling command '''
+MULTIPLE_READ ='27000322271083'
+
 STOP_READ     ='28000028'
+
+'''section: 2.12 Set Working Place'''
+SET_REGION_EU = '070001030B' #for Setting EU Region
+SET_REGION_US = '070001020A' #for Setting US Region
+
+'''Section: 2.16 Get transmitting power '''
+GET_TRANSMIT_PWR = 'B70000B7'
 
 class UHF():
     def __init__(self,baudrate):
@@ -54,11 +69,30 @@ class UHF():
 
     def stop_read(self):
         data = self.send_command([STARTBYTE, STOP_READ, ENDBYTE])
-
+    
+    def setRegion_EU(self):
+        data = self.send_command([STARTBYTE, SET_REGION_EU, ENDBYTE])
+        time.sleep(0.5)
+        rec_data = self.serial.read(24)
+        #print(rec_data)    
+    
+    def setRegion_US(self):
+        data = self.send_command([STARTBYTE, SET_REGION_US, ENDBYTE])
+        time.sleep(0.5)
+        rec_data = self.serial.read(24)
+        #print(rec_data)
+        
+    def getTransmit_Power(self):
+        data = self.send_command([STARTBYTE, GET_TRANSMIT_PWR, ENDBYTE])
+        time.sleep(0.5)
+        rec_data = self.serial.read(24)
+        print(rec_data)
+        
     def single_read(self):
         data = self.send_command([STARTBYTE, SINGLE_READ, ENDBYTE])
         time.sleep(0.5)
         rec_data = self.serial.read(24)
+        #print(rec_data)
         if rec_data is not None and len(rec_data)>22:
             if rec_data[0] != 0xbb or rec_data[23] != 0x7e or rec_data[1] != 0x02:
                 return None        
